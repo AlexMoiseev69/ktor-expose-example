728335ee7f749e086ecf5d668746446412fe3a7e
Alexander Moiseev <alexander.moiseev@zalando.de>
2021-10-20 22:17:27 +0200

    first commit

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,19 @@
+package com.example
+
+import com.example.plugins.configureHTTP
+import com.example.plugins.configureMonitoring
+import com.example.plugins.configureRouting
+import com.example.plugins.configureSecurity
+import com.example.plugins.configureSerialization
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSecurity()
+        configureMonitoring()
+        configureSerialization()
+        configureHTTP()
+    }.start(wait = true)
+}
---
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -0,0 +1,53 @@
+package com.example.plugins
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.OAuthAccessTokenResponse
+import io.ktor.auth.OAuthServerSettings
+import io.ktor.auth.authenticate
+import io.ktor.auth.authentication
+import io.ktor.auth.oauth
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.http.HttpMethod
+import io.ktor.response.respondRedirect
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.sessions.sessions
+
+fun Application.configureSecurity() {
+
+    install(Authentication) {
+        oauth("auth-oauth-google") {
+            urlProvider = { "http://localhost:8080/callback" }
+            providerLookup = {
+                OAuthServerSettings.OAuth2ServerSettings(
+                    name = "google",
+                    authorizeUrl = "https://accounts.google.com/o/oauth2/auth",
+                    accessTokenUrl = "https://accounts.google.com/o/oauth2/token",
+                    requestMethod = HttpMethod.Post,
+                    clientId = System.getenv("GOOGLE_CLIENT_ID"),
+                    clientSecret = System.getenv("GOOGLE_CLIENT_SECRET"),
+                    defaultScopes = listOf("https://www.googleapis.com/auth/userinfo.profile")
+                )
+            }
+            client = HttpClient(Apache)
+        }
+    }
+
+    routing {
+        authenticate("auth-oauth-google") {
+            get("login") {
+                call.respondRedirect("/callback")
+            }
+
+            get("/callback") {
+                val principal: OAuthAccessTokenResponse.OAuth2? = call.authentication.principal()
+                call.sessions.set("", principal?.accessToken.toString()) // TODO: fix it
+                call.respondRedirect("/hello")
+            }
+        }
+    }
+}
